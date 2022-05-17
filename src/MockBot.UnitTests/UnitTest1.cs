2f1cd57d3a267e40e04fb3228ef661ed8a7bf03d
Martin Kearn <martin.kearn@microsoft.com>
2022-05-17 17:17:46 +0100

    Throw away unit test

--- src/MockBot.UnitTests/UnitTest1.cs
+++ src/MockBot.UnitTests/UnitTest1.cs
@@ -4,10 +4,12 @@ namespace MockBot.UnitTests
 {
     public class UnitTest1
     {
+        // This just a throw-away test to use until we write proper tests for this repo
         [Fact]
         public void Test1()
         {
-
+            var foo = "bar";
+            Assert.Equal("bar", foo);
         }
     }
 }
