63bcc12a3910fa68417923f4d959101776bea29b
Martin Kearn <martin.kearn@microsoft.com>
2022-05-18 11:05:26 +0100

    Values controller and get test

---
+++ src/MockBot.Api/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// This is a sample controller used only for the purposes of having some code to test until the actual solution gets built
+// Without this, we could not pass the code coverage requirements for the CI pipeline
+// This should be removed when actual solution code gets added
+
+namespace MockBot.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET: api/<ValuesController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ValuesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ValuesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ValuesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ValuesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/MockBot.UnitTests/ValuesControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
+using MockBot.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MockBot.UnitTests
+{
+    public class ValuesControllerTests
+    {
+        private readonly ValuesController sut;
+
+        public ValuesControllerTests()
+        {
+            sut = new ValuesController();
+        }
+
+        [Fact]
+        public void GetReturnsExpected()
+        {
+            // Arrange
+
+            // Act
+            var result = sut.Get();
+
+            // Assert
+            Assert.Equal(new string[] { "value1", "value2" }, result);
+    }
+    }
+}
