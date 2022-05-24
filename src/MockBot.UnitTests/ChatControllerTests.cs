b62b65018fcf7a90373b6f16e645fcd43a537cec
Joonas Roosalu <joonas.roosalu@eesti.ee>
2022-05-24 09:56:43 +0300

    feat: #10 Create findAllChats test

--- src/MockBot.UnitTests/ChatControllerTests.cs
+++ src/MockBot.UnitTests/ChatControllerTests.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using MockBot.Api.Controllers;
 using MockBot.Api.Interfaces;
 using MockBot.Api.Models;
@@ -10,11 +13,26 @@ namespace MockBot.UnitTests
     {
         private readonly ChatController sut;
         private readonly Mock<IChatService> mockChatService;
+        private readonly Dictionary<Guid, Chat> _chats;
 
         public ChatControllerTests()
         {
             mockChatService = new Mock<IChatService>();
             sut = new ChatController(mockChatService.Object);
+            _chats = new Dictionary<Guid, Chat>();
+        }
+
+        [Fact]
+        public void FindAll()
+        {
+            _ = mockChatService.Setup(mock => mock.CreateChat()).Returns(new Chat());
+            var chat = sut.Post();
+            _chats.Add(chat.Id, chat);
+
+            _ = mockChatService.Setup(mock => mock.FindAll()).Returns(_chats.Values.ToList());
+            var findAllChats = sut.FindAll();
+
+            Assert.Equal(_chats.Count, findAllChats.Count());
         }
 
         [Fact]
